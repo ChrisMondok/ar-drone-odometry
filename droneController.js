1807cd8ac38337c4035121020d58072042b5a216
Chris Mondok <chris.mondok@gmail.com>
2014-05-26 11:46:21 -0400

    Face the sticker. Comment out other stuff.

--- droneController.js
+++ droneController.js
@@ -3,19 +3,17 @@
 var arDrone = require('ar-drone');
 
 function droneController(drone) {
-	this.js = new(require('joystick'))(0,3500,350);
+	this.js = new(require('joystick'))(0,10000,350);
 
 	this.js.on('button', this.buttonPressed.bind(this));
 	this.js.on('axis', this.axisMoved.bind(this));
 
 	this.drone = drone ||  new (arDrone).createClient();
 
-	this.drone.config('general:navdata_demo', 'FALSE');
-
 	this.drone.on('navdata',this.gotNavData.bind(this));
 
-	var control = arDrone.createUdpControl();
-	setInterval(function() { control.flush(); }, 30);
+	this.control = arDrone.createUdpControl();
+	setInterval(this.tick.bind(this), 30);
 
 	this.cardinal = false;
 	this.log = false;
@@ -23,9 +21,43 @@ function droneController(drone) {
 
 	this.latestNavData = null;
 
+	this.front = this._lastFront = 0;
+	this.right = this._lastRight = 0;
+	this.up = this._lastUp = 0;
+	this.clockwise = this._lastClockwise = 0;
+
 	this.joyX = 0;
 	this.joyY = 0;
+
+	this.faceSticker = false;
 }
+
+droneController.prototype.tick = function() {
+	if(this.front === 0 && this.right === 0 && ( this.front != this._lastFront || this.right != this._lastRight)) {
+		this.drone.stop();
+	}
+
+	if(this.front != this._lastFront)
+		this.drone.front(this.front);
+	
+	if(this.right != this._lastRight)
+		this.drone.right(this.right);
+
+	if(this.up != this._lastUp)
+		this.drone.up(this.up);
+
+	if(this.clockwise != this._lastClockwise)
+		this.drone.clockwise(this.clockwise);
+
+	console.log(this.front, this.right, this.up, this.clockwise);
+
+	this._lastUp = this.up;
+	this._lastRight = this.right;
+	this._lastFront = this.front;
+	this._lastClockwise = this.clockwise;
+
+	this.control.flush();
+};
 
 droneController.prototype.buttonPressed = function(event) {
 	if(event.value)
@@ -33,6 +65,10 @@ droneController.prototype.buttonPressed = function(event) {
 			case 0:
 				console.log("Stop");
 				this.drone.stop();
+				break;
+			case 3:
+				console.log('Toggle facing');
+				this.toggleFacing();
 				break;
 			case 6:
 				console.log("Land");
@@ -68,53 +104,64 @@ droneController.prototype.axisMoved = function(event) {
 			else
 				this.joyX = event.value/32768;
 
-		var right = this.joyX * Math.cos(heading) - this.joyY * Math.sin(heading);
-		var front = -(this.joyX * Math.sin(heading) + this.joyY * Math.cos(heading));
-		
-		this.drone.right(right);
-		this.drone.front(front);
+		this.right = this.joyX * Math.cos(heading) - this.joyY * Math.sin(heading);
+		this.front = -(this.joyX * Math.sin(heading) + this.joyY * Math.cos(heading));
 	}
 	else
 	{
 		switch(event.number) {
 			case 0: //horizontal axis
-				if(event.value < 0)
-					this.drone.left(-event.value/32767);
-				else
-					this.drone.right(event.value/32767);
+				this.right = event.value/32767;
 				break;
-
 			case 1: //vertical axis
-				if(event.value > 0)
-					this.drone.back(event.value/32767);
-				else
-					this.drone.front(-event.value/32767);
+				this.front = -event.value/32767;
 				break;
 		}
 	}
 
 	switch(event.number) {
 		case 3:
-			this.drone.clockwise(event.value/32767);
+			if(!this.faceSticker)
+				this.clockwise = event.value/32767;
 			break;
 		case 5:
-			var value = (event.value+32767)/65535;
-			this.drone.up(value);
+			this.up = (event.value+32767)/65535;
 			break;
 		case 2:
-			var value = (event.value+32767)/65535;
-			this.drone.down(value);
+			this.up = -((event.value+32767)/65535);
 			break;
 	}
-}
+};
 
 droneController.prototype.gotNavData = function(navdata) {
 	this.latestNavData = navdata;
 	if(this.log)
 		this.loggedEvents.push(navdata);
+	if(this.faceSticker) {
+		this.clockwise = 0;
+		if('visionDetect' in navdata) {
+			if(navdata.visionDetect.nbDetected) {
+				var v = navdata.visionDetect;
+				var x = ((v.xc[0] / 1000) - 0.5)*2;
+				var y = ((v.yc[0] / 1000) - 0.5)*2;
+
+				console.log(x, y);
+
+				this.clockwise = x;
+			}
+			else
+				console.warn("Sticker not detected");
+		}
+		else
+			console.warn("visionDetect not in navdata");
+	}
 
 
-}
+};
+
+droneController.prototype.toggleFacing = function() {
+	this.faceSticker = !this.faceSticker;
+};
 
 droneController.prototype.toggleLogging = function() {
 	this.log = !this.log;
@@ -123,6 +170,6 @@ droneController.prototype.toggleLogging = function() {
 		require('fs').writeFile('navdata',JSON.stringify(this.loggedEvents));
 		this.loggedEvents = [];
 	}
-}
+};
 
 exports.droneController = droneController;
--- fly.js
+++ fly.js
@@ -1,30 +1,45 @@
 var drone = new (require('ar-drone')).createClient();
-var website = new (require('telemetry')).website();
+//var website = new (require('telemetry')).website();
 var controller = new (require('droneController')).droneController(drone);
-var odometry = new(require('odometry')).odometry();
+//var odometry = new(require('odometry')).odometry();
+
+var lastNavdata = null;
 
 drone.on('navdata',gotNavData);
 
-var pngStream = drone.createPngStream();
+lookForOrangeAndBlueStickers();
 
-pngStream.on('data',gotImage)
+//var pngStream = drone.createPngStream();
+//
+//pngStream.on('data',gotImage)
+
+
+function lookForOrangeAndBlueStickers() {
+	drone.config('detect:detect_type', 10);
+	drone.config('detect:enemy_colors', 3);
+	drone.config('detect:detections_select_h',1);
+}
 
 function gotNavData(navdata) {
 
-	odometry.update(navdata)
+	lastNavdata = navdata;
 
-	var message = {
-		north:odometry.north,
-		east:odometry.east,
-	}
+//	odometry.update(navdata)
+//
+//	var message = {
+//		north:odometry.north,
+//		east:odometry.east,
+//	}
 
-	if('demo' in navdata)
+	if('demo' in navdata) {
 		message.battery = navdata.demo.batteryPercentage
+		console.log("Battery: %s",message.battery);
+	}
 
 	if('magneto' in navdata)
 		message.heading = navdata.magneto.heading.fusionUnwrapped/180*Math.PI;
 
-	website.broadcastData(message);
+//	website.broadcastData(message);
 }
 
 function gotImage(image) {
